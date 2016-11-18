d5866b606559ec8338e0fd7b5afe89b69f9a1a20
Robert Stiff <uatecuk@gmail.com>
2016-11-18 19:50:21 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Assembly.GetCallingAssembly().FullName);
+        }
+    }
+}
